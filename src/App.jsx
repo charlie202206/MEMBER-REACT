3ff7aa4916f8a7bea98c67c57d9ddac3789db16d
charlie202206 <donghu@sk.com>
2022-08-18 17:58:06 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import NewMemberList from './pages/NewMemberList'
 // import Login from './pages/login'
 import { ContextProvider } from "./ContextAPI";
 import React from 'react'
-// import {BrowserRouter as Router, Route, Switch} from "react"
-// import About from "./pages/About"
-// import Profile from "./pages/Profile"
+
 import Login from './pages/login'
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import NewMember from './pages/newMember';
 import NewMemberList from './pages/NewMemberList';
 import SaveMember from './pages/SaveMember';
-import Test2 from './pages/Test2';
-import Test from './pages/Test';
 // import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -21,8 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route element={<NewMember/>} path="/NewMember"   />
       <Route element={<NewMemberList/>} path="/NewMemberList"  />
       <Route element={<SaveMember/>} path="/SaveMember"  />
-      <Route element={<Test2 lbg="Volvo"/>} path="/Test2"/>
-      <Route element={<Test/>} path="/Test"/>
     </Routes>
     </BrowserRouter>
   // </React.StrictMode>
